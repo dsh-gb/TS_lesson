a9ffe921ccc17d9e0f9bf92b4f1538811a8d30ae
dsh-gb <shamonov.dima@gmail.com>
2022-02-27 12:24:17 +0300

    lesson-6

--- src/user-data.ts
+++ src/user-data.ts
@@ -1,38 +1,36 @@
 import { Favorites, instanceOfFavorites, instanceOfUser, User } from './type.js';
 
 export function getUserData(): User {
-  const user: unknown = JSON.parse(localStorage.getItem('user')) // читаем из localStorage запись user
-
-  if (user == null) {
-    // создаем запись user в localStorage для неизвестного пользователя и возвращаем его из ф-и
-    const unknownUser: User = {
-      name: 'unknown name',
-      avatarUrl: 'img/avatar-unknown.png'
-    }
+  const userData = localStorage.getItem('user') // читаем из localStorage запись user
+  const unknownUser: User = {
+    name: 'unknown name',
+    avatarUrl: 'img/avatar-unknown.png'
+  }
+  const setUnknownUser = () => {
     localStorage.setItem('user', JSON.stringify(unknownUser))
     return unknownUser
   }
-
-  // если прочитанная из localStorage запись user соотвествует типу User, то возвращаем из ф-и эту запись
-  if (instanceOfUser(user)) {
-    return user
-  }
+  if (userData !== null) {
+    const user: unknown = JSON.parse(userData)
+    if (instanceOfUser(user)) {
+      return user // если прочитанная из localStorage запись user соотвествует типу User, то возвращаем из ф-и эту запись
+    } else return setUnknownUser() // иначе записываем в localStorage unknownUser и возвращаем это значение
+  } else return setUnknownUser()
 }
 
 export function getFavoritesAmount(): Favorites {
-  const favorites: unknown = JSON.parse(localStorage.getItem('favorites')) // читаем из localStorage запись favorites
-
-  if (favorites == null) {
-    // создаем запись favorites в localStorage для неизвестного избранного и возвращаем его из ф-и
-    const unknownFavorites: Favorites = {
-      amount: 0
-    }
+  const favoritesData = localStorage.getItem('favorites') // читаем из localStorage запись favorites
+  const unknownFavorites: Favorites = {
+    amount: 0
+  }
+  const setUnknownFavorites = () => { // создаем запись favorites в localStorage для неизвестного избранного и возвращаем его из ф-и
     localStorage.setItem('favorites', JSON.stringify(unknownFavorites))
     return unknownFavorites
   }
-
-  // если прочитанная из localStorage запись favorites соотвествует типу Favorites, то возвращаем из ф-и эту запись
-  if (instanceOfFavorites(favorites)) {
-    return favorites
-  }
+  if (favoritesData !== null) {
+    const favorites: unknown = JSON.parse(favoritesData)
+    if (instanceOfFavorites(favorites)) {
+      return favorites // если прочитанная из localStorage запись favorites соотвествует типу Favorites, то возвращаем из ф-и эту запись
+    } else return setUnknownFavorites() // иначе записываем в localStorage unknownFavorites и возвращаем это значение
+  } else return setUnknownFavorites()
 }
